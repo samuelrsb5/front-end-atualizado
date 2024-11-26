029acff6f1aac8080128a02763ff74ceba7ab4a5
samuelrsb5 <samuelcomgeorge@gmail.com>
2024-11-25 21:08:34 -0300

    Envio do estilo do mapa

--- src/app/mapa-eco/mapa-eco.component.ts
+++ src/app/mapa-eco/mapa-eco.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mapa-eco',
-  templateUrl: './mapa-eco.component.html',
-  styleUrl: './mapa-eco.component.css'
+    selector: 'app-mapa-eco',
+    templateUrl: './mapa-eco.component.html',
+    styleUrl: './mapa-eco.component.css',
+    standalone: false
 })
-export class MapaEcoComponent {
+export class MapaEcoComponent implements OnInit {
+  center: google.maps.LatLngLiteral = { lat: -8.0476, lng: -34.8770 }; // Exemplo: Recife
+  zoom = 12;
+  markers: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://seuprojeto.com/api/locais_coleta.php')
+      .subscribe(locais => {
+        this.markers = locais.map(local => ({
+          position: { lat: local.latitude, lng: local.longitude },
+          title: local.nome,
+          info: local.descricao
+        }));
+      });
+  }
 }
